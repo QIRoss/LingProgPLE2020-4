75c738bf47425d3b5f3d87aaa3597a3537f46409
Lucas de Queiroz <lucasqreis@gmail.com>
2020-09-17 05:41:11 -0300

    all functions

--- Trabalho1/caos.cpp
+++ Trabalho1/caos.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+vector<string> ufs = {
+        "AC","AL","AM","AP","BA","CE","DF","ES","GO",
+        "MA","MG","MS","MT","PA","PB","PE","PI","PR",
+        "RJ","RN","RO","RR","RS","SC","SE","SP","TO"
+    };
+
 int 
 *estadualDeaths(string initialData, string finalData, string uf){
     int* r = new int[2];
@@ -75,26 +81,21 @@ deathRate(string initialDate, string finalDate, string initialDateLinha, string 
     return deathRate;
 }
 
-vector<string> ufs = {
-        "AC","AL","AM","AP","BA","CE","DF","ES","GO",
-        "MA","MG","MS","MT","PA","PB","PE","PI","PR",
-        "RJ","RN","RO","RR","RS","SC","SE","SP","TO"
-    };
 
 vector<int>
-calcAllPercentages(){
+calcAllPercentages(string initialDate, string finalDate, string initialDateLinha, string finalDateLinha){
     unsigned index=0;
     vector<int> percents(27);
     for (index=0;index<ufs.size();index++){
-        percents[index] = deathRate("2020-01-01","2020-08-06","2020-01-01","2020-09-07",ufs[index]);
+        percents[index] = deathRate(initialDate,finalDate,initialDateLinha,finalDateLinha,ufs[index]);
     }
     return percents;
 }
 
 vector<string>
-allStatus(){
+allStatus(string initialDate, string finalDate, string initialDateLinha, string finalDateLinha){
     vector<string> showStatus(27);
-    vector<int> percents = calcAllPercentages();
+    vector<int> percents = calcAllPercentages(initialDate,finalDate,initialDateLinha, finalDateLinha);
     unsigned index;
     for (index=0;index<ufs.size();index++){
         if(percents[index] < -15){
@@ -108,13 +109,78 @@ allStatus(){
     return showStatus;
 }
 
+void
+brazilStatus(string initialDate,string finalDate, string initialDateLinha, string finalDateLinha){
+    float deaths=0;
+    float deathsLinha=0;
+    int *results;
+    float index;
+    float totalDays = 0;
+    float totalDaysLinha = 0;
+    float media;
+    float mediaLinha;
+    for(index=0;index<ufs.size();index++){
+        results = estadualDeaths(initialDate,finalDate, ufs[index]);
+        deaths+=results[0];
+        totalDays=results[1];
+    }
+    for(index=0;index<ufs.size();index++){
+        results = estadualDeaths(initialDateLinha,finalDateLinha, ufs[index]);
+        deathsLinha+=results[0];
+        totalDaysLinha=results[1];
+    }
+    media = deaths/totalDays;
+    mediaLinha = deathsLinha/totalDaysLinha;
+    float rate = (mediaLinha/media)*100 - 100;
+    string status;
+    if (rate > 15){
+        status = "ALTA";
+    } else if (rate < -15){
+        status = "BAIXA";
+    } else {
+        status = "ESTÁVEL";
+    }
+
+    cout << endl << "Mortes no Brasil ate de " <<initialDate<< " a " << finalDate << ": " << deaths << endl
+    << "Dias: " << totalDays << endl
+    << "Media: " << deaths/totalDays << endl
+    << "Mortes no Brasil ate de " <<initialDateLinha<< " a " << finalDateLinha 
+    << ": " << deathsLinha << endl
+    << "Dias: " << totalDaysLinha << endl
+    << "Media: " << deathsLinha/totalDaysLinha << endl
+    << "Variação: " << rate << "%" << endl
+    << "Status: " << status << endl;
+}
+
+void higherAndLower(string initialDate, string finalDate, string initialDateLinha, string finalDateLinha){
+    vector<int> percents = calcAllPercentages(initialDate,finalDate, initialDateLinha, finalDateLinha);
+    int index=0;
+    int higher=percents[0];
+    int higherIndex=0;
+    int lowerIndex=0;
+    int lower=percents[0];
+    for(index=1;index<ufs.size();index++){
+        if(percents[index] > higher){
+            higher = percents[index];
+            higherIndex = index;
+        }
+        if(percents[index] < lower){
+            lower = percents[index];
+            lowerIndex = index;
+        }
+    }
+    cout << "Estado com maior aumento é " << ufs[higherIndex] << " com " << higher << "% de aumento" << endl << "Estado com maior baixa é " << ufs[lowerIndex] << " com " << lower << "% de aumento" << endl;
+}
+
 int
 main(int argc,char *argv[]){
     unsigned index;
-    vector<string> status = allStatus();
-    vector<int> percents = calcAllPercentages();
+    vector<string> status = allStatus("2020-07-07","2020-08-07","2020-08-07","2020-09-07");
+    vector<int> percents = calcAllPercentages("2020-07-07","2020-08-07","2020-08-07","2020-09-07");
     for(index=0;index<ufs.size();index++){
         cout << ufs[index] << "\t" << percents[index] << "%\t" << status[index] << endl;
     }
+    brazilStatus("2020-07-07","2020-08-07","2020-08-07","2020-09-07");
+    higherAndLower("2020-07-07","2020-08-07","2020-08-07","2020-09-07");
     return 0;
 }
