43225cb334cd21e363ff3a480115e9c8448da073
Lucas de Queiroz <lucasqreis@gmail.com>
2020-10-09 17:52:18 -0300

    node

--- TrabalhoGrafos/main.cpp
+++ TrabalhoGrafos/main.cpp
@@ -11,13 +11,31 @@ using namespace std;
 #define OK                                      0
 #define NUMERO_ARGUMENTOS_INVALIDO              1
 
+struct node{
+    string word;
+    
+    node* next[];
+};
+
+class Graph {
+    int vertex;
+    vector<vector<node>> **i;
+};
+
 int main(int argc, char *argv[]){
     fstream file; 
     string word, t, q, filename; 
     filename = argv[1];
     vector<string> phrase;
     unsigned index=0;
-    file.open(filename.c_str()); 
+
+    if(argc != NUMERO_ARGUMENTOS){
+        printf("Uso %s: <NomeDoArquivo.txt\n",argv[0]);
+        exit(NUMERO_ARGUMENTOS_INVALIDO);
+    }
+
+    file.open(filename.c_str());
+
     while (file >> word) { 
         phrase.push_back(word);
         cout << phrase[index] << endl;
@@ -25,7 +43,5 @@ int main(int argc, char *argv[]){
     }
     file.close();
 
-
-    cout << "something" << endl;
     return OK;
 }
