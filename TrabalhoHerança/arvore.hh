59ff5781668ededd1db69a8916a0f3a6379cd5db
Lucas de Queiroz <lucasqreis@gmail.com>
2020-10-23 15:08:01 -0300

    templating

---
+++ TrabalhoHerança/arvore.hh
@@ -0,0 +1,16 @@
+#ifndef _ARVORE_HH_
+#define _ARVORE_HH_         "arvore.hh"
+
+template <class T>
+class Arvore{
+    public:
+        Arvore();
+        ~Arvore();
+
+        bool insere();
+        bool busca();
+        bool imprime();
+    private:
+};
+
+#endif
